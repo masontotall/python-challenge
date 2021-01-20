15fb0d858281501d3090fc251c9e2cf70942e996
masontotall <68935404+masontotall@users.noreply.github.com>
2021-01-20 12:08:46 -0600

    this is the basic code to read the csvfile with our information

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,21 @@
 print("Hello from PyBank")
+# make files accessible across platforms
+import os
+
+#Read CSV mod
+import csv
+
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+
+#improved reading
+
+with open(csvpath) as csvfile:
+
+    csvreader=csv.reader(csvfile, delimiter=",")
+
+    for row in csvreader:
+        print(row)
+
+
+
+
