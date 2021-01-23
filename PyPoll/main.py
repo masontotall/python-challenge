f27afb719b1415e6d3310f4bb1a5978e7bdaec23
masontotall <68935404+masontotall@users.noreply.github.com>
2021-01-23 17:48:39 -0600

    Read the csv file and calculated total number of vote

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,29 @@
 print("Hello from PyPoll")
+# make files accessible across platforms
+import os
+
+#Read CSV mod
+import csv
+
+csvpath = os.path.join('..', 'Resources', 'election_data.csv')
+
+#improved reading
+
+with open(csvpath) as csvfile:
+
+    csvreader=csv.reader(csvfile, delimiter=",")
+
+    csv_header = next(csvreader)
+    print(f'CSV Header: {csv_header}')
+
+    for row in csvreader:
+        print(row)
+
+    
+with open(csvpath) as csvfile:
+    csvreader=csv.reader(csvfile, delimiter=",")
+    
+    csv_header = next(csvreader)
+
+    total_votes=sum(1 for row in csvreader)
+    print(total_votes)
